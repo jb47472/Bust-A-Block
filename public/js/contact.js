fbc483ef286f6c10b30103c1665139f8f657721a
Mona Lisa <jb47472@hotmail.com>
2019-05-07 19:39:29 -0400

    fuck it

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,22 +1,3 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
-
-// create the connection information for the sql database
-var connection = mysql.createConnection({
-    host: "localhost",
-  
-    // Your port; if not 3306
-    port: 3306,
-  
-    // Your username
-    user: "root",
-  
-    // Your password
-    password: "root",
-    database: "newUser_db"
-  });
-  
-  
   $("#send").on("click", function(event) {
         event.preventDefault();
         // Form validation
