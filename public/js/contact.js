17cf3424047104b0be324861e9ffbbd4d1bedcd8
Mona Lisa <jb47472@hotmail.com>
2019-05-07 19:32:12 -0400

    screw git

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,18 +1,37 @@
-    $("#send").on("click", function(event) {
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "root",
+    database: "newUser_db"
+  });
+  
+  
+  $("#send").on("click", function(event) {
         event.preventDefault();
         // Form validation
-        function validateForm() {
-          var isValid = true;
-          $(".input").each(function() {
-            if ($(this).val() === "") {
-              isValid = false;
-            }
-          });
-          return isValid;
-        }
+        // function validateForm() {
+        //   var isValid = true;
+        //   $(".input").each(function() {
+        //     if ($(this).val() === "") {
+        //       isValid = false;
+        //     }
+        //   });
+        //   return isValid;
+        // }
 
          // If all required fields are filled
-      if (validateForm()) {
+    //   if (validateForm()) {
         var newUser = {
             name: $("#name").val(),
             email: $("#email").val(),
@@ -21,5 +40,5 @@
             message: $("#message").val(),
         }
             console.log(newUser);
-    }
-})
+
+    })
