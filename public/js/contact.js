fa21b64f6c231dc43dd23287426fc0874be38917
Mona Lisa <jb47472@hotmail.com>
2019-05-08 19:18:01 -0400

    new changes

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,25 +1,17 @@
-  $("#send").on("click", function(event) {
-        event.preventDefault();
-        // Form validation
-        // function validateForm() {
-        //   var isValid = true;
-        //   $(".input").each(function() {
-        //     if ($(this).val() === "") {
-        //       isValid = false;
-        //     }
-        //   });
-        //   return isValid;
-        // }
 
-         // If all required fields are filled
-    //   if (validateForm()) {
+ $("#submit").on("click", function(event) {
+        event.preventDefault();
+      
         var newUser = {
             name: $("#name").val(),
             email: $("#email").val(),
             phone: $("#phone").val(),
             subject: $("#subject").val(),
             message: $("#message").val(),
+           
         }
+    
+        
             console.log(newUser);
 
     })
