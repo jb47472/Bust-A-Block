674d97f67f9bc383b11356a3980d7bce9a9d0078
Mona Lisa <jb47472@hotmail.com>
2019-05-08 21:02:56 -0400

    working validation and post rout to update MySQL

--- public/js/contact.js
+++ public/js/contact.js
@@ -10,6 +10,12 @@
             message: $("#message").val(),
            
         }
+      
+        
+        // function createNewUser() {
+          $.post("/api/contacts", newUser)
+            .then(window.location.href = 'http://localhost:3000/survey');
+        // }
     
         
             console.log(newUser);
