ead8859216f9dd39361222f9b9933c94fcb2775a
glennbackus <46578905+glennbackus@users.noreply.github.com>
2019-05-09 18:09:34 -0400

    pull

--- movie randomize/app/data/movies.js
+++ movie randomize/app/data/movies.js
@@ -1,4 +1,5 @@
 var movies = [
+        
     {
         name: "Batman",
         Photo: "https://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg",
@@ -8,9 +9,130 @@ var movies = [
             "2",
             "5",
             "2",
-            "1",
+            "1"
         ]
-    }, {
+    }, 
+    {
+        name: "Psycho",
+        photo: "https://www.imdb.com/title/tt0054215/mediaviewer/rm1902135552",
+        scores: [
+            "1",
+            "2",
+            "3",
+            "1",
+            "2",
+            "1"
+        ]
+        },
+    {
+        name: "Dr. Strangelove",
+        photo: "https://www.imdb.com/title/tt0057012/mediaviewer/rm3960095232",
+        scores: [
+            "2",
+            "5",
+            "2",
+            "1",
+            "2",
+            "1"
+
+        ]
+    },
+    {
+        name: "The Graduate",
+        photo: "https://www.imdb.com/title/tt0061722/mediaviewer/rm4046423296",
+        scores: [
+            "2",
+            "5",
+            "3",
+            "1",
+            "2",
+            "1"
+        ]
+    },
+    {
+        name: "Planet of the Apes",
+        photo: "https://www.imdb.com/title/tt0063442/mediaviewer/rm3327645440",
+        scores: [
+            "5",
+            "3",
+            "3",
+            "1",
+            "2",
+            "1"
+
+        ]
+    },
+    {
+        name: "Rosemary's Baby",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjL1bC9tY_iAhWKct8KHcHABkgQjRx6BAgBEAU&url=http%3A%2F%2Fgoogle.com%2Fsearch%3Ftbm%3Disch%26q%3DRosemary%2527s%2BBaby&psig=AOvVaw3Z2wXNiKoVVNKbx6zGvTAj&ust=1557524613043279",
+        scores: [
+            "1",
+            "2",
+            "4",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        name: "In the Heat of the Night",
+        photo: "https://www.imdb.com/title/tt0061811/mediaviewer/rm2580760320",
+        scores: [
+            "6",
+            "2",
+            "2",
+            "1",
+            "2",
+            "1"
+        ]
+    },
+    
+    {
+        name: "Easy Rider",
+        photo: "https://www.imdb.com/title/tt0064276/mediaviewer/rm1288191232",
+        scores: [
+            "3",
+            "2",
+            "2",
+            "1",
+            "2",
+            "1"
+
+        ]
+    },
+    {
+        name: "The Good, the Bad and the Ugly",
+        photo: "https://en.wikipedia.org/wiki/The_Good,_the_Bad_and_the_Ugly#/media/File:Good_the_bad_and_the_ugly_poster.jpg",
+        scores: [
+            "3",
+            "2",
+            "4",
+            "1",
+            "1",
+            "1"
+
+        ]
+    },
+    {
+        name: "Mary Poppins",
+        photo: "https://www.imdb.com/title/tt0058331/mediaviewer/rm2777561856",
+        scores: [
+            "2",
+            "3",
+            "4",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        name: "West Side Story",
+        photo: "https://www.imdb.com/title/tt0055614/mediaviewer/rm3878337280",
+        scores: [
+
+        ]
+    }
+    {
         name: "lilo & stitch",
         Photo: "http://www.media2.hw-static.com/wp-content/uploads/lilo-stitch-movie-still-lilo-stitch-poster_1109887-300x392.jpeg",
         scores: [
